87b884058b45773839657c9731309219fa7ecba0
Darshit152002 <darshi2002shah@gmail.com>
2025-02-28 15:36:03 +0530

    movie

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ require("dotenv").config();
 const app = express();
 const dbConfig = require("./config/dbConfig");
 const userRoutes = require("./routes/userRoutes");
+const movieRoutes = require("./routes/movieRoutes");
+// const theatreRoutes = require("./routes/theatreRoutes");
+// const showRoutes = require("./routes/showRoutes");
 
 app.use(express.json());
 app.use("/api/users", userRoutes.router);
+app.use("/api/movies", movieRoutes.router);
+// app.use("/api/theatre", theatreRoutes.router);
+// app.use("/api/shows", showRoutes.router);
 
 app.listen(8080, () => {
   console.log("Server has started");
